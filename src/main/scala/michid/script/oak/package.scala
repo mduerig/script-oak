e0421ed6f5a72ad083536adc5fe6e5f3b0fd1a73
mduerig <mduerig@apache.org>
2017-01-18 14:56:13 +0100

    Support for BlobStore

--- src/main/scala/michid/script/oak/package.scala
+++ src/main/scala/michid/script/oak/package.scala
@@ -2,10 +2,23 @@ package michid.script
 
 import ammonite.ops._
 import michid.script.oak.filestore.FileStoreAnalyser
+import org.apache.jackrabbit.oak.plugins.blob.datastore.{DataStoreBlobStore, OakFileDataStore}
+import org.apache.jackrabbit.oak.segment.file.tooling.BasicReadOnlyBlobStore
 import org.apache.jackrabbit.oak.spi.blob.BlobStore
 
 /** Common predefs used by script-oak */
 package object oak {
+
+  /** A blob store that actually ignores the binaries. Useful if the blob store is not available  */
+  val dummyBlobStore: BlobStore = new BasicReadOnlyBlobStore
+
+  /** A file data store based blob store */
+  def newBlobStore(directory: Path): BlobStore = {
+    val delegate = new OakFileDataStore
+    delegate.setPath(directory.toString)
+    delegate.init(null)
+    new DataStoreBlobStore(delegate)
+  }
 
   /** Create a new file store analyser */
   def fileStoreAnalyser(
