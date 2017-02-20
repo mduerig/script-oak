ae5da179d01d9baa2258008641c656bc6aef25cf
mduerig <mduerig@apache.org>
2017-02-20 17:58:25 +0100

    Trying to introduce ITs. No success so far with redirecting IO

---
+++ script-oak-it/src/test/scala/ScriptOakIntegrationTest.scala
@@ -0,0 +1,56 @@
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import java.nio.charset.StandardCharsets
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{Outcome, fixture}
+
+@RunWith(classOf[JUnitRunner])
+class ScriptOakIntegrationTest extends fixture.FunSuite {
+  type FixtureParam = String
+
+  override def withFixture(test: OneArgTest): Outcome = {
+    val fixture = "a fixture object"
+    try test(fixture)
+    finally {}
+  }
+
+  test("Main.run") { _ => // michid can't get redirecting io to work properly
+    val i = new ByteArrayInputStream("println(42)\nexit\n".getBytes(StandardCharsets.UTF_8))
+    val o = new ByteArrayOutputStream
+    val e = new ByteArrayOutputStream
+
+    michid.script.shell.Main(i, o, e).run()
+
+    println("<out>" + o.toString + "</out>")
+    println("<err>" + e.toString + "</err>")
+  }
+
+  test("Main.runCode") { _ =>  // michid can't get redirecting io to work properly
+    val i = new ByteArrayInputStream(new Array[Byte](0))
+    val o = new ByteArrayOutputStream
+    val e = new ByteArrayOutputStream
+
+    michid.script.shell.Main(i, o, e).runCode("println(42)", replApi = true)
+
+    println("<out>" + o.toString + "</out>")
+    println("<err>" + e.toString + "</err>")
+  }
+
+  test("Main.main") { _ =>  // michid can't get redirecting io to work properly
+    val i = new ByteArrayInputStream("println(42)\nexit\n".getBytes(StandardCharsets.UTF_8))
+    val o = new ByteArrayOutputStream
+    val e = new ByteArrayOutputStream
+
+    Console.withIn(i) {
+      Console.withOut(o) {
+        Console.withErr(e) {
+          ammonite.Main.main(new Array[String](0))
+        }
+      }
+    }
+
+    println("<out>" + o.toString + "</out>")
+    println("<err>" + e.toString + "</err>")
+  }
+}
--- script-oak-shell/src/main/scala/michid/script/shell/Main.scala
+++ script-oak-shell/src/main/scala/michid/script/shell/Main.scala
@@ -1,14 +1,26 @@
 package michid.script.shell
 
+import java.io.{InputStream, OutputStream}
+
 /**
   * Main class for an interactive Scala shell including all Oak dependencies
   */
 object Main {
-  def main(args: Array[String]): Unit = {
-    val version = getClass.getPackage.getImplementationVersion
+  def main(args: Array[String]): Unit = Main().run()
+
+  def apply(inputStream: InputStream = System.in,
+            outputStream: OutputStream = System.out,
+            errorStream: OutputStream = System.err
+           ): ammonite.Main = {
+    val version:String =
+      Option(getClass.getPackage.getImplementationVersion)
+            .getOrElse("latest.integration")
     ammonite.Main(
+      predef = s"import $$ivy.`michid:script-oak:$version`, michid.script.oak._",
       welcomeBanner = Some(s"Welcome to Script Oak $version"),
-      predef = s"import $$ivy.`michid:script-oak:$version`, michid.script.oak._"
-    ).run()
+      inputStream = inputStream,
+      outputStream = outputStream,
+      errorStream = errorStream
+    )
   }
 }
