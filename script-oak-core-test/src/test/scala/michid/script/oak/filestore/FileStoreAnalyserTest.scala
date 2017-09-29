a97bd7d451f17e92c4ec1ca116d92bf310a602e9
mduerig <mduerig@apache.org>
2017-09-30 00:42:28 +0200

    FileStoreAnalyserTest

---
+++ script-oak-core-test/src/test/scala/michid/script/oak/filestore/FileStoreAnalyserTest.scala
@@ -0,0 +1,35 @@
+package michid.script.oak.filestore
+
+import michid.script.oak.fixture.{EmptyFileStore, fileStoreAnalyser}
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import ammonite.ops._
+
+@RunWith(classOf[JUnitRunner])
+class FileStoreAnalyserTest extends FunSuite {
+  object AccessMode extends Enumeration {
+    type AccessMode = Value
+    val ReadWrite, ReadOnly = Value
+  }
+  import AccessMode._
+
+  AccessMode.values.foreach { accessMode =>
+
+    def withFSA(test: FileStoreAnalyser => Any): Unit = {
+      val directory = EmptyFileStore.path/up
+      val fsa = fileStoreAnalyser(directory/"segmentstore", directory/"datastore", accessMode == ReadOnly)
+      try {
+        test(fsa)
+      }
+      finally {
+        fsa.close()
+      }
+    }
+
+    ignore(s"Create file store analyser ($accessMode)") {  // michid fix for read only mode
+      withFSA { identity }
+    }
+
+  }
+}
