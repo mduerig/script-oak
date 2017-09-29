b66624fb953da5593f04e070cb82c93ca84e7aab
mduerig <mduerig@apache.org>
2017-09-29 13:53:50 +0200

    Factor out fixtures

---
+++ script-oak-fixture-reactor/script-oak-fixtures/src/main/scala/michid/script/oak/fixtures/package.scala
@@ -0,0 +1,36 @@
+package michid.script.oak
+
+import java.lang.System.getProperties
+
+import ammonite.interp.InterpBridge
+import coursier.{Dependency, Module}
+import coursier.maven.MavenRepository
+
+package object fixtures {
+  val scriptOakVersion: String = Option(getClass.getPackage.getImplementationVersion)
+        .getOrElse("1.3-SNAPSHOT")  // michid latest.integration equivalent?
+
+  object oak_1_7_7 {
+    val oakVersion = "oak-1.7.7"
+
+    // michid .m2 resolve should be there by default
+    val predef: String = ("""
+      |interp.repositories() ++= Seq(coursier.MavenRepository("file://" + java.lang.System.getProperties.get("user.home") + "/.m2/repository/"))
+      |interp.load.ivy(coursier.Dependency("michid"%"script-""" + oakVersion + """", """" + scriptOakVersion + """"))
+      |@
+      |import michid.script.oak._
+      |import michid.script.oak.fixture._
+    |""").stripMargin
+
+    def load(): Unit = {
+      val interpreter = InterpBridge.value0
+      if (interpreter != null) {
+        interpreter.repositories() ++= Seq(MavenRepository("file://" + getProperties.get("user.home") + "/.m2/repository/"))
+        interpreter.load.ivy(Dependency(Module("michid", s"script-$oakVersion"), scriptOakVersion))
+      } else {
+        throw new Error("No interpreter")
+      }
+    }
+  }
+
+}
--- script-oak-shell/src/main/scala/michid/script/shell/Main.scala
+++ script-oak-shell/src/main/scala/michid/script/shell/Main.scala
@@ -1,31 +1,24 @@
 package michid.script.shell
 
+import michid.script.oak.fixtures.{oak_1_7_7, scriptOakVersion}
+
 /**
   * Main class for an interactive Scala shell including all Oak dependencies
   */
 object Main {
-  val scriptOakVersion: String = Option(getClass.getPackage.getImplementationVersion)
-          .getOrElse("1.3-SNAPSHOT")  // michid latest.integration equivalent?
-
-  // michid .m2 resolve should be there by default
-  private val predef: String = ("""
-      |interp.repositories() ++= Seq(coursier.MavenRepository("file://" + java.lang.System.getProperties.get("user.home") + "/.m2/repository/"))
-      |interp.load.ivy(coursier.Dependency("michid"%"script-oak-1.7.7", """" + scriptOakVersion +""""))
-      |@
-      |import michid.script.oak._
-      |import michid.script.oak.fixture._
-  |""").stripMargin
+  // michid inject via command line args, add good default
+  private val fixture = oak_1_7_7
 
   def main(args: Array[String]): Unit = {
     ammonite.Main.main(Array(
-      "--predef-code", predef,
-      "--banner", "Welcome to Script Oak " + scriptOakVersion) ++
+      "--predef-code", fixture.predef,
+      "--banner", s"Welcome to Script Oak $scriptOakVersion / ${fixture.oakVersion}") ++
       args)
   }
 
   def run(script: String): Unit =
     println(ammonite.Main.main0(List(
-      "--predef-code", predef,
+      "--predef-code", fixture.predef,
       "--code", script),
       System.in, System.out, System.err))
 }
