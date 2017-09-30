9a7b11e2a3dced5eee9077bb6f618677c14c7c0b
mduerig <mduerig@apache.org>
2017-09-30 23:54:57 +0200

    Simplify

--- script-oak-fixture-reactor/script-oak-1.7.8/src/main/scala/michid/script/oak/fixture/package.scala
+++ script-oak-fixture-reactor/script-oak-1.7.8/src/main/scala/michid/script/oak/fixture/package.scala
@@ -69,7 +69,7 @@ package object fixture {
   object EmptyFileStore {
     val path: Path = {
       val path = pwd / "target" / Random.nextInt(1000000).toString / "segmentstore"
-      fileStoreAnalyser(path, readOnly = false).close()
+      plainFileStoreBuilder(path).build().close()
       path
     }
   }
